559fdd9d3ad2f6793d21084a41695f0519613833
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-07 11:34:39 +0200

    Minor changes made

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,6 @@
+<script setup>
+   const user = ref('')
+</script>
 <template>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/">Blog Webpage</a>
