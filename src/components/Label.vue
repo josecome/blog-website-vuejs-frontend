33e832e0604764a15f88dda7d1d7e6e5d6fb42a5
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-07 11:32:31 +0200

    Label component added

---
+++ src/components/Label.vue
@@ -0,0 +1,28 @@
+<template>
+    <label id="lbl" >
+      {{ text }}
+    </label>
+</template>
+  
+  <script>
+  export default {
+    name: 'Label',
+    props: {
+      text: String
+    }
+  }
+  </script>
+  <style scoped>
+    #lbl {
+      width: 100%;  
+      border: 1px solid gray; 
+      border-radius: 6px;
+      text-align: right;  
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 20px;
+      list-style: none;
+      padding: 10px 12px;
+      display: inline-block;
+    }
+  </style>
