aabb1132b60cb4549c177a330dc4f8259dd7ed87
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-07 13:21:36 +0200

    Refs to load contents from server added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+        const posts = ref([])
+        const postlikes = ref([]);
+        const postcomments = ref([]);
+        const useratributes = ref([]);
+        const postshares = ref([]);
+        const commenttxt = ref('');        
+        const posts_user = ref([]);
+        const initialState = ref(false);
+        
+</script>
 
 <template>
   <main></main>
